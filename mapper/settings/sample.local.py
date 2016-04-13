bdd32e506f03b539ec6d0acbc9a9ca5f6feb824c
yarara <y.ulanovich@yandex.ru>
2016-04-13 21:28:31 +0300

    add sample.local.py and README

---
+++ mapper/settings/sample.local.py
@@ -0,0 +1,14 @@
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE':'',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        }
+}
+
