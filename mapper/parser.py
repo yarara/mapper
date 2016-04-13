7a0532f106f2b8c7d540245d5f1eab2fff4df4de
yarara <y.ulanovich@yandex.ru>
2016-04-13 10:05:03 +0300

    fix parser

--- mapper/parser.py
+++ mapper/parser.py
@@ -3,119 +3,146 @@ import sys
 from datetime import datetime
 # from mapper.events.models import Event, Place, Schedule
 
-file = open('/home/yaraat/projects/mapper/mapper/test.xml')
-file_text = file.read()
-file.close()
+def get_xml():
+    file = open('/home/yaraat/projects/mapper/mapper/test.xml')
+    file_text = file.read()
+    file.close()
 
-if sys.version_info.major == 3:
-    xml = etree.fromstring(file_text.encode('utf-8'))
-else:
-    xml = etree.fromstring(file_text)
+    if sys.version_info.major == 3:
+        xml = etree.fromstring(file_text.encode('utf-8'))
+    else:
+        xml = etree.fromstring(file_text)
 
-events = xml.find('events')
-places = xml.find('places')
-schedule = xml.find('schedule')
+    return xml
 
-for event in events:
-    event_dict = dict()
-    if 'id' in event.attrib.keys():
-        event_dict['id'] = event.attrib['id']
-    if 'price' in event.attrib.keys():
-        event_dict['price'] = event.attrib['price']
-    if 'type' in event.attrib.keys():
-        event_dict['event_type'] = event.attrib['type']
-    if event.find('title'):
-        event_dict['title'] = event.find('title').text
-    if event.find('age_restricted'):
-        event_dict['age_restricted'] = event.find('age_restricted').text
-    if event.find('tags'):
-        event_dict['tags'] = '; '.join([tag.text for tag in event.find('tags').findall('tag')])
-    if event.find('persons'):
-        event_dict['persons'] = [{'name': person.find('name').text, 'role': person.find('role').text} for person in event.find('persons').findall('person')]
-    if event.find('text'):
-        event_dict['text'] = event.find('text').text
-    if event.find('description'):
-        event_dict['description'] = event.find('description').text
-    if event.find('stage_theatre'):
-        event_dict['stage_theatre'] = event.find('stage_theatre')
-    if event.find('gallery'):
-        event_dict['image_url'] = '; '.join([image.attrib['href'] for image in event.find('gallery').findall('image')])
+def get_events(events):
+    for event in events:
+        event_dict = dict()
+        if 'id' in event.attrib.keys():
+            event_dict['id'] = event.attrib['id']
+        if 'price' in event.attrib.keys():
+            event_dict['price'] = event.attrib['price']
+        if 'type' in event.attrib.keys():
+            event_dict['event_type'] = event.attrib['type']
 
-    print(event_dict.keys())
-    print(event_dict.values())
+        title = event.find('title')
+        age_restricted = event.find('age_restricted')
+        tags = event.find('tags')
+        persons = event.find('persons')
+        text = event.find('text')
+        description = event.find('description')
+        stage_theatre = event.find('stage_theatre')
+        gallery = event.find('gallery')
 
-    # new_event = Event()
-    # for key in event_dict.keys():
-    #     # if hasattr(new_event, key):
-    #     setattr(new_event, key, event_dict[key])
-    # new_event.save()
+        if title  is not None:
+            event_dict['title'] = title.text
+        if age_restricted  is not None:
+            event_dict['age_restricted'] = age_restricted.text
+        if tags  is not None:
+            event_dict['tags'] = '; '.join([tag.text for tag in tags.findall('tag')])
+        if persons  is not None:
+            event_dict['persons'] = [{'name': person.find('name').text, 'role': person.find('role').text} for person in persons.findall('person')]
+        if text  is not None:
+            event_dict['text'] = text.text
+        if description  is not None:
+            event_dict['description'] = description.text
+        if stage_theatre  is not None:
+            event_dict['stage_theatre'] = stage_theatre.text
+        if gallery is not None:
+            event_dict['image_url'] = '; '.join([image.attrib['href'] for image in gallery.findall('image')])
 
-for place in places:
-    place_dict = dict()
-    if 'id' in place.attrib.keys():
-        place_dict['id'] = place.attrib['id']
-    if 'type' in place.attrib.keys():
-        place_dict['place_type'] = place.attrib['type']
-    if place.find('city'):
-        place_dict['city'] = place.find('city').text
-    if place.find('title'):
-        place_dict['title'] = place.find('title').text
-    if place.find('address'):
-        place_dict['address'] = place.find('address').text
-    if place.find('coordinates'):
-        place_dict['latitude'] = place.find('coordinates').attrib['latitude']
-        place_dict['longitude'] = place.find('coordinates').attrib['longitude']
-    if place.find('phones'):
-        place_dict['phones'] = '; '.join([phone.text for phone in place.find('phones').findall('phone')])
-    if place.find('tags'):
-        place_dict['tags'] = '; '.join([tag.text for tag in place.find('tags').findall('tag')])
-    if place.find('metros'):
-        place_dict['metros'] = '; '.join([metro.text for metro in place.find('metros').findall('metro')])
-    if place.find('gallery'):
-        place_dict['image_url'] = '; '.join([image.attrib['href'] for image in place.find('gallery').findall('image')])
-    if place.find('text'):
-        place_dict['text'] = place.find('text').text
-    if place.find('url'):
-        place_dict['url'] = place.find('url').text
-    if place.find('work_times'):
-        for work_time in place.find('work_times').findall('work_time'):
-            if work_time.attrib['type'] == 'kassa':
-                place_dict['work_time_kassa'] = work_time.text
-            if work_time.attrib['type'] == 'openhours':
-                place_dict['work_time_openhours'] = work_time.text
-            if work_time.attrib['type'] == 'other':
-                place_dict['work_time_other'] = work_time.text
+        # print(event_dict.keys())
+        # print(event_dict.values())
 
-    print(place_dict)
-    # new_place = Place()
-    # for key in place_dict.keys():
-    #     if hasattr(new_place, key):
-    #         setattr(new_place, key, place_dict[key])
-    # new_place.save()
+        new_event = Event()
+        for key in event_dict.keys():
+            # if hasattr(new_event, key):
+            setattr(new_event, key, event_dict[key])
+        new_event.save()
 
-for session in schedule.findall('session'):
-    session_dict = dict()
-    session_dict['event'] = session.attrib['event']
-    session_dict['place'] = session.attrib['place']
-    session_dict['date'] = datetime.strptime(session.attrib['date'], "%Y-%m-%d").date()
-    session_dict['time'] = session.attrib['time']
-    if 'timetill' in session.attrib.keys():
-        session_dict['timetill'] = session.attrib['timetill']
+def get_places(places):
+    for place in places:
+        place_dict = dict()
+        if 'id' in place.attrib.keys():
+            place_dict['id'] = place.attrib['id']
+        if 'type' in place.attrib.keys():
+            place_dict['place_type'] = place.attrib['type']
 
-    print(session_dict)
+        city = place.find('city')
+        title = place.find('title')
+        address = place.find('address')
+        coordinates = place.find('coordinates')
+        phones = place.find('phones')
+        tags = place.find('tags')
+        metros = place.find('metros')
+        gallery = place.find('gallery')
+        text = place.find('text')
+        url = place.find('url')
+        work_times = place.find('work_times')
 
-    # new_session = Schedule()
-    # for key in session_dict.keys():
-    #     if hasattr(new_session, key):
-    #         setattr(new_session, key, session_dict[key])
-    # new_session.save()
+        if city is not None:
+            place_dict['city'] = city.text
+        if title is not None:
+            place_dict['title'] = title.text
+        if address is not None:
+            place_dict['address'] = address.text
+        if coordinates is not None:
+            place_dict['latitude'] = coordinates.attrib['latitude']
+            place_dict['longitude'] = coordinates.attrib['longitude']
+        if phones is not None:
+            place_dict['phones'] = '; '.join([phone.text for phone in phones.findall('phone')])
+        if tags is not None:
+            place_dict['tags'] = '; '.join([tag.text for tag in tags.findall('tag')])
+        if metros is not None:
+            place_dict['metros'] = '; '.join([metro.text for metro in metros.findall('metro')])
+        if gallery is not None:
+            place_dict['image_url'] = '; '.join([image.attrib['href'] for image in gallery.findall('image')])
+        if text is not None:
+            place_dict['text'] = text.text
+        if url is not None:
+            place_dict['url'] = url.text
+        if work_times is not None:
+            for work_time in work_times.findall('work_time'):
+                if work_time.attrib['type'] == 'kassa':
+                    place_dict['work_time_kassa'] = work_time.text
+                if work_time.attrib['type'] == 'openhours':
+                    place_dict['work_time_openhours'] = work_time.text
+                if work_time.attrib['type'] == 'other':
+                    place_dict['work_time_other'] = work_time.text
+
+        # print(place_dict)
+        new_place = Place()
+        for key in place_dict.keys():
+            if hasattr(new_place, key):
+                setattr(new_place, key, place_dict[key])
+        new_place.save()
+
+def get_schedule(schedule):
+    for session in schedule.findall('session'):
+        session_dict = dict()
+        session_dict['event'] = session.attrib['event']
+        session_dict['place'] = session.attrib['place']
+        session_dict['date'] = datetime.strptime(session.attrib['date'], "%Y-%m-%d").date()
+        session_dict['time'] = session.attrib['time']
+        if 'timetill' in session.attrib.keys():
+            session_dict['timetill'] = session.attrib['timetill']
+
+        # print(session_dict)
+
+        new_session = Schedule()
+        for key in session_dict.keys():
+            if hasattr(new_session, key):
+                setattr(new_session, key, session_dict[key])
+        new_session.save()
 
 
-# def print_elem(xml):
-# 	if xml.getchildren():
-# 		for children in xml.getchildren():
-# 			print_elem(children)
-# 	else:
-# 		a = '{0} -- {1}'.format(xml.tag.encode('utf-8'), xml.text.encode('utf-8') if xml.text else 'empty')
-# 		print(a)
+def parser():
+    xml = get_xml()
+    events = xml.find('events')
+    places = xml.find('places')
+    schedule = xml.find('schedule')
+    get_events(events)
+    get_places(places)
+    get_schedule(schedule)
+
 
